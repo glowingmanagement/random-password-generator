138fc425607898261dde71e5664c043a9719443b
Josh Holmes <josh@glowingmanagement.com>
2022-06-04 18:38:43 +0100

    generate random password

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,57 @@
 let chosenCharacters = [];
 let passwordLength = 8;
 let websiteChosen = "";
+const characterChoice = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "0",
+  " ",
+  "!",
+  "£",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "#",
+  "@",
+];
+let generatedPassword = [];
 
 // onSubmit view user data and add to variables
 const handleSubmit = (event) => {
@@ -13,12 +64,25 @@ const handleSubmit = (event) => {
     "special-choice",
   ];
   chosenCharacters = [];
+  generatedPassword = [];
   name.forEach((element) => isChecked(element));
   passwordLength = $("#character-length").val();
   websiteChosen = $("#websiteInput").val();
 
   const isValidated = validateForm();
   console.log(isValidated);
+
+  if (isValidated) {
+    for (let i = 0; i < passwordLength - chosenCharacters.length; i += 1) {
+      // choose random number between 1 & length of array to determine which character type to use
+      const randomCharacter = getRandomNumber(0, chosenCharacters.length);
+      console.log(chosenCharacters);
+      const passwordCharacter = chosenCharacters[randomCharacter];
+
+      getRandomCharacters(passwordCharacter, generatedPassword);
+    }
+    console.log(generatedPassword);
+  }
 };
 
 const isChecked = (name) => {
@@ -52,6 +116,31 @@ const validateForm = () => {
 };
 
 // generate random password
+
+const addEachCharacter = () => {};
+
+const getRandomCharacters = (passwordCharacter, generatedPassword) => {
+  // get random character of the chosen type and add it to array
+  if (passwordCharacter === "lowercase-choice") {
+    generatedPassword.push(characterChoice[getRandomNumber(0, 26)]);
+  } else if (passwordCharacter === "uppercase-choice") {
+    generatedPassword.push(
+      characterChoice[getRandomNumber(0, 26)].toUpperCase()
+    );
+  } else if (passwordCharacter === "numeric-choice") {
+    generatedPassword.push(characterChoice[getRandomNumber(26, 36)]);
+  } else if (passwordCharacter === "special-choice") {
+    generatedPassword.push(
+      characterChoice[getRandomNumber(36, characterChoice.length)]
+    );
+  }
+};
+
+const getRandomNumber = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.ceil(max);
+  return Math.floor(Math.random() * (max - min) + min);
+};
 
 // store password in local storage
 

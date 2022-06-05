c9d64a4a875788c9f94513f4f93400cd7686b822
Josh Holmes <josh@glowingmanagement.com>
2022-06-05 07:55:18 +0100

    HTML of saved password and toggle password icon

--- assets/js/savedPassword.js
+++ assets/js/savedPassword.js
@@ -1,3 +1,37 @@
+const readFromLocalStorage = (key, defaultValue) => {
+  // get from LS using key name
+  const dataFromLS = localStorage.getItem(key);
+
+  // parse data from LS
+  const parsedData = JSON.parse(dataFromLS);
+
+  if (parsedData) {
+    return parsedData;
+  } else {
+    return defaultValue;
+  }
+};
+
+const togglePassword = () => {
+  const passwordIcon = $("#togglePassword");
+  if (passwordIcon.hasClass("fa-eye-slash")) {
+    passwordIcon.removeClass("fa-eye-slash");
+    passwordIcon.addClass("fa-eye");
+  } else {
+    passwordIcon.addClass("fa-eye-slash");
+    passwordIcon.removeClass("fa-eye");
+  }
+};
+
+const onReady = () => {
+  const data = readFromLocalStorage();
+  console.log(data);
+};
+
+onReady();
+
+$("#togglePassword").click(togglePassword);
+
 $("#homeBtn").click(function () {
   location.href = "../../index.html";
 });
